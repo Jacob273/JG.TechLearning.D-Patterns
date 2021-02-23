e57a7a4abcf69b125f874c6a5a07ca493e9d7291
jacob273 <jakubgmur@op.pl>
2021-02-23 23:27:16 +0100

    fixed CQRS.QueryHandlers.csproj unloaded

---
+++ JG.TechLearning.D-Patterns/JG.TechLearning.CQRS.QueryHandlers/GetOrderQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using JG.TechLearning.CQRS.Queries;
+using JG.TechLearning.DTO;
+using JG.TechLearning.ICommon.CQRS;
+using Microsoft.Extensions.Logging;
+
+namespace JG.TechLearning.CQRS.QueryHandlers
+{
+    public class GetOrderQueryHandler : IQueryHandler<GetOrderQuery, OrderDTO>
+    {
+
+        ILogger _logger;
+
+        public GetOrderQueryHandler(ILogger<GetOrderQueryHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public OrderDTO Handle(IQuery<OrderDTO> query)
+        {
+            _logger.LogInformation("GetOrderQueryHandler:: getting the order...");
+
+            return new OrderDTO("Order-1");
+        }
+    }
+}
